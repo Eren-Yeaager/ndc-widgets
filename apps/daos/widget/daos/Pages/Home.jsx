9ed004139b51b73a466e8485c967b0ff5a35c2c7
Andrii Furmanets <afurm@Andriis-MacBook-Air.local>
2024-03-28 13:39:48 +0200

    List of supported projects must be updated.

--- apps/daos/widget/daos/Pages/Home.jsx
+++ apps/daos/widget/daos/Pages/Home.jsx
@@ -168,13 +168,28 @@ let proposals = Near.view(contractName, "get_all_posts", {
   limit: 100,
 });
 
-let projects = Near.view(contractName, "get_dao_communities", {
+let projects = []
+
+let projectsDaoId2 = Near.view(contractName, "get_dao_communities", {
   dao_id: parseInt(2),
 });
+
+// Marketing DAO
+let projectsDaoId4 = Near.view(contractName, "get_dao_communities", {
+  dao_id: parseInt(4),
+});
+
+// Gaming DAO
+let projectsDaoId3 = Near.view(contractName, "get_dao_communities", {
+  dao_id: parseInt(3),
+});
+
 
 
 if (!daos || !contractName || !content || !assets || !proposals || !projects)
   return <Widget src="flashui.near/widget/Loading" />;
+
+projects = [...projectsDaoId2, ...projectsDaoId4, ...projectsDaoId3]  
 
 let groupedDaos = daos
   .map((element) => {
